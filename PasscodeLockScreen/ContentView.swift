6ed6d026a6b7b3b0ab4bf56002b34c3b1b627852
Leonard-McCook <leonard.mccookcarr@outlook.com>
2024-02-21 01:04:38 -0500

    Initial Commit

---
+++ PasscodeLockScreen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PasscodeLockScreen
+//
+//  Created by Leonard McCook-Carr on 2/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PasscodeLockScreen/PasscodeLockScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasscodeLockScreenApp.swift
+//  PasscodeLockScreen
+//
+//  Created by Leonard McCook-Carr on 2/21/24.
+//
+
+import SwiftUI
+
+@main
+struct PasscodeLockScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
