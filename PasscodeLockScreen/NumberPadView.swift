6ba680e50274232178c0df513a6b7255d1592df4
Leonard-McCook <leonard.mccookcarr@outlook.com>
2024-02-22 18:24:48 -0500

    Create view files

---
+++ PasscodeLockScreen/NumberPadView.swift
@@ -0,0 +1,18 @@
+//
+//  NumberPadView.swift
+//  PasscodeLockScreen
+//
+//  Created by Leonard McCook-Carr on 2/22/24.
+//
+
+import SwiftUI
+
+struct NumberPadView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NumberPadView()
+}
---
+++ PasscodeLockScreen/PasscodeIndicatorView.swift
@@ -0,0 +1,18 @@
+//
+//  PasscodeIndicatorView.swift
+//  PasscodeLockScreen
+//
+//  Created by Leonard McCook-Carr on 2/22/24.
+//
+
+import SwiftUI
+
+struct PasscodeIndicatorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PasscodeIndicatorView()
+}
